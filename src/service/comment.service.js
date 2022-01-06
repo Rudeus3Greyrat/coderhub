5898adbf5054050c43b8cc7455cabfb15fecb404
Rudeus3Greyrat <3041084465@qq.com>
2022-01-06 14:05:17 +0800

    feat: 增加动态列表中展示评论个数功能

--- src/service/comment.service.js
+++ src/service/comment.service.js
@@ -2,8 +2,7 @@ const connection = require('../app/database')
 
 class CommentService {
     async create(userId, content,momentId) {
-        const statement = `INSERT INTO comment (content, user_id,moment_id)
-                           values (?, ?, ?);`
+        const statement = `INSERT INTO comment (content, user_id,moment_id) values (?, ?, ?);`
         const [result] = await connection.execute(statement, [content, userId,momentId])
         return result
     }
--- src/service/moment.service.js
+++ src/service/moment.service.js
@@ -1,15 +1,14 @@
 const connection = require('../app/database')
 
-const sqlFragment=`
-SELECT m.id       id,
-                                  m.content  content,
-                                  m.createAt createTime,
-                                  m.updateAt updateTime,
-                                  JSON_OBJECT('id', u.id, 'name', u.name) author
-                           FROM
-                               moment m
-                               LEFT JOIN user u
-                           on u.id = m.user_id`
+const sqlFragment = `
+    SELECT m.id                                    id,
+           m.content                               content,
+           m.createAt                              createTime,
+           m.updateAt                              updateTime,
+           JSON_OBJECT('id', u.id, 'name', u.name) author
+    FROM moment m
+             LEFT JOIN user u
+                       on u.id = m.user_id`
 
 class MomentService {
     async create(userId, content) {
@@ -26,21 +25,33 @@ class MomentService {
         return result[0]
     }
 
-    async getMomentList(offset,size){
-        const statement=`${sqlFragment}
-                           LIMIT ?,?;`
-        const [result] = await connection.execute(statement, [offset,size])
+    async getMomentList(offset, size) {
+        const statement = `
+            SELECT m.id                                                      id,
+                   m.content                                                 content,
+                   m.createAt                                                createTime,
+                   m.updateAt                                                updateTime,
+                   JSON_OBJECT('id', u.id, 'name', u.name)                   author,
+                   (SELECT COUNT(*) FROM comment c WHERE c.moment_id = m.id) commentCount
+            FROM moment m
+                     LEFT JOIN user u
+                               on u.id = m.user_id LIMIT ?,?;`
+        const [result] = await connection.execute(statement, [offset, size])
         return result
     }
 
-    async update(momentId,content){
-        const statement=`UPDATE moment SET content = ? where id = ?;`
-        const [result] = await connection.execute(statement, [content,momentId])
+    async update(momentId, content) {
+        const statement = `UPDATE moment
+                           SET content = ?
+                           where id = ?;`
+        const [result] = await connection.execute(statement, [content, momentId])
         return result
     }
 
-    async remove(momentId){
-        const statement=`DELETE FROM moment WHERE id = ?;`
+    async remove(momentId) {
+        const statement = `DELETE
+                           FROM moment
+                           WHERE id = ?;`
         const [result] = await connection.execute(statement, [momentId])
         return result
     }
